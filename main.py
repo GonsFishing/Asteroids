447518249b300cf3ccae662cd28cd6591157c22d
GonsFishing <sixtenglarsson@gmail.com>
2025-04-02 14:28:09 +0200

    Added groups

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
 import pygame
-from constants import * 
+from constants import *
+from player import *
 def main():
     pygame.init()
+    drawables = pygame.sprite.Group()
+    updatables = pygame.sprite.Group()
+    Player.containers = (drawables, updatables)
+    dt = 0
+    clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        updatables.update(dt)
         screen.fill("black")
+        drawables.draw(screen)
+
         pygame.display.flip()
-
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
-
+        dt = clock.tick(60) / 1000
 if __name__ == "__main__":
     main()
